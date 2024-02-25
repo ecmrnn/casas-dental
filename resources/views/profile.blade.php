df96838aab5350b3ee33d5d303e53161db6a54c0
Emanuelle Maranan <marananemanuelle@gmail.com>
2024-02-26 02:20:54 +0800

    Created static tasks and small cards

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
         {{ __('Profile') }}
     </x-slot>
 
-    <div class="py-12">
+    <div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white border border-gray-200 sm:rounded-lg">
                 <div class="max-w-xl">
